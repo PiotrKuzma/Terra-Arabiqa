d032a28e56422435708cc7da98a61e1e065c6e77
unknown <kuzma.boh@gmail.com>
2020-04-22 18:01:13 +0200

    Improved styling of components

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,94 +1,79 @@
-import React from 'react'
+import React from "react";
 
-import './login.style.scss'
-import Input from '../../components/input/input'
-import UniButton from '../../components/uniButton/uniButton'
+import "./login.style.scss";
+import Input from "../../components/input/input";
+import UniButton from "../../components/uniButton/uniButton";
 
-import { signInWithGoogle, auth } from '../../firebase/firebase.utils'
+import { signInWithGoogle, auth } from "../../firebase/firebase.utils";
 
 class Login extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            email: '',
-            password: ''
-        }
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
 
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const { email, password } = this.state;
 
-       
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        email: "",
+        password: "",
+      });
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    handleSubmit = async event => {
-        event.preventDefault()
-        const { email, password } = this.state
+  handleChange = (event) => {
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
 
-        try {
-            await auth.signInWithEmailAndPassword(email, password)
-            this.setState( {
-                email: "",
-                password: ""
-            })
+  render() {
+    return (
+      <div className="login">
+        <h2 className="login__title">Posiadam konto</h2>
+        <span className="login__span">Zaloguj się za pomocą email i hasła</span>
+        <form className="form" onSubmit={this.handleSubmit}>
+          <label className="form__label">Email</label>
+          <Input
+            className="form__input"
+            name="email"
+            type="email"
+            value={this.state.email}
+            required
+            handleChange={this.handleChange}
+          />
 
-        }
+          <label className="form__label">Hasło</label>
+          <Input
+            className="form__input"
+            name="password"
+            type="password"
+            value={this.state.password}
+            required
+            handleChange={this.handleChange}
+          />
 
-        catch (error){
-            console.log(error)
-        }
-        
-    }
-
-    handleChange = event => {
-        const {value, name} = event.target
-        this.setState( {
-            [name]: value
-        })
-    }
-
-    render() {
-        return(
-            <div className="login">
-                <h2 className="login__title">Posiadam konto</h2>
-                <span className="login__span">Zaloguj się za pomocą email i hasła</span>
-                <form className="form"
-                onSubmit={this.handleSubmit}
-                
-                >
-                    <Input className="form__input"
-                    name="email" 
-                    type="email" 
-                    value={this.state.email} 
-                    required
-                    handleChange={this.handleChange}
-                    
-                    />
-
-                    <label className="form__label">Email</label>
-                    <Input className="form__input"
-                    name="password" 
-                    type ="password" 
-                    value={this.state.password} 
-                    required
-                    handleChange={this.handleChange}
-                    
-                    
-                    />
-
-                    <label className="form__label">Hasło</label>
-
-                    <UniButton className="unibutton" type="submit" >Zaloguj</UniButton>
-                    <UniButton className="unibutton" onClick={signInWithGoogle} >Google</UniButton>
-                    
-                    
-                    
-                   
-                
-                </form>
-            
-            </div>
-
-        )
-    }
+          <UniButton className="unibutton" type="submit">
+            Zaloguj
+          </UniButton>
+          <UniButton className="unibutton" onClick={signInWithGoogle}>
+            Zaloguj z Google
+          </UniButton>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default Login
+export default Login;
--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,109 +1,102 @@
-import React from 'react'
-import './register.style.scss'
-import Input from '../input/input'
-import UniButton from '../uniButton/uniButton'
-import {auth, createProfileDoc } from '../../firebase/firebase.utils'
+import React from "react";
+import "./register.style.scss";
+import Input from "../input/input";
+import UniButton from "../uniButton/uniButton";
+import { auth, createProfileDoc } from "../../firebase/firebase.utils";
 
 class Register extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            displayName: '',
-            email: '',
-            password: '',
-            passwordConfirm: ''
-        }
+    this.state = {
+      displayName: "",
+      email: "",
+      password: "",
+      passwordConfirm: "",
+    };
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const { displayName, email, password, passwordConfirm } = this.state;
+    if (password != passwordConfirm) {
+      alert("Hasła nie zgadzają się.");
+      return;
     }
 
-    handleSubmit = async event => {
-        event.preventDefault()
-        const {displayName, email, password,passwordConfirm } = this.state
-        if (password != passwordConfirm) {
-            alert("Hasła nie zgadzają się.")
-            return
-        }
-
-        try {
-            const {user} = await auth.createUserWithEmailAndPassword(email, password)
-            await createProfileDoc(user, {displayName})
-            this.setState( {
-                displayName: '',
-                email: '',
-                password: '',
-                passwordConfirm: ''
-            })
-
-        }
-        catch (error) {
-            console.error(error)
-        }
-
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      await createProfileDoc(user, { displayName });
+      this.setState({
+        displayName: "",
+        email: "",
+        password: "",
+        passwordConfirm: "",
+      });
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    handleChange = event => {
-        const {name, value } = event.target
-        this.setState({[name]: value})
-    }
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
 
-    render() {
-        const {displayName, email, password,passwordConfirm } = this.state
-        return (
-            <div className="register">
-                <h2 className="register__title">Nie posiadam konta</h2>
-                <span className="register__span">Utwórz nowe konto</span>
-                <form className="form" onSubmit={this.handleSubmit}>
-                    <Input className="form__input"
-                        type="text"
-                        name="displayName"
-                        value={displayName}
-                        onChange={this.handleChange}
-                        required
-      
-                        >
-                    </Input>
-                    <label className="form__label">Użytownik</label>
-                    <Input className="form__input"
-                        type="email"
-                        name="email"
-                        value={email}
-                        onChange={this.handleChange}
-                        required
-      
-                        >
-                    </Input>
-                    <label className="form__label">Adres Email</label>
-                    <Input className="form__input"
-                        type="password"
-                        name="password"
-                        value={password}
-                        onChange={this.handleChange}
-                        required
-      
-                        >
-                    </Input>
-                    <label className="form__label">Hasło</label>
-                    <Input className="form__input"
-                        type="password"
-                        name="passwordConfirm"
-                        value={passwordConfirm}
-                        onChange={this.handleChange}
-                        required
-      
-                        >
-                    </Input>
-                    <label className="form__label">Potwierdź hasło</label>
+  render() {
+    const { displayName, email, password, passwordConfirm } = this.state;
+    return (
+      <div className="register">
+        <h2 className="register__title">Nie posiadam konta</h2>
+        <span className="register__span">Utwórz nowe konto</span>
+        <form className="form" onSubmit={this.handleSubmit}>
+          <label className="form__label">Użytownik</label>
+          <Input
+            className="form__input"
+            type="text"
+            name="displayName"
+            value={displayName}
+            onChange={this.handleChange}
+            required
+          ></Input>
+          <label className="form__label">Adres Email</label>
+          <Input
+            className="form__input"
+            type="email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+            required
+          ></Input>
+          <label className="form__label">Hasło</label>
+          <Input
+            className="form__input"
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+            required
+          ></Input>
+          <label className="form__label">Potwierdź hasło</label>
+          <Input
+            className="form__input"
+            type="password"
+            name="passwordConfirm"
+            value={passwordConfirm}
+            onChange={this.handleChange}
+            required
+          ></Input>
 
-                    <UniButton className="unibutton" type="submit">Zarejestruj</UniButton>
-            
-            
-            
-                </form>
-            
-            </div>
-        )
-    }
+          <UniButton className="unibutton" type="submit">
+            Zarejestruj
+          </UniButton>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default Register
-
+export default Register;
