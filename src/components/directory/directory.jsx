87d54cb7c7e755b7dd9bb38838b7b3e5059a442f
unknown <kuzma.boh@gmail.com>
2020-05-24 19:13:20 +0200

    Ui improvements: typography, colors

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -9,28 +9,28 @@ class Directory extends React.Component {
     this.state = {
       category: [
         {
-          title: "kawa prażona",
+          title: "Kawa prażona",
           imageUrl:
             "https://images.unsplash.com/photo-1524350876685-274059332603?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80",
           id: 1,
           linkUrl: "kawa",
         },
         {
-          title: "kawiarki",
+          title: "Kawiarki",
           imageUrl:
             "https://images.unsplash.com/photo-1491755276353-97642ec79167?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80",
           id: 2,
           linkUrl: "kawiarki",
         },
         {
-          title: "akcesoria",
+          title: "Akcesoria",
           imageUrl:
             "https://images.unsplash.com/photo-1515592302748-6c5ea17e2f0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
           id: 3,
           linkUrl: "akcesoria",
         },
         {
-          title: "zestawy",
+          title: "Zestawy",
           imageUrl:
             "https://images.unsplash.com/photo-1439242088854-0c76045f4124?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
 
