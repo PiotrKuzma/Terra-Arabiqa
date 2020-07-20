5cfc598986321ddd6ea4dd6b89bc8e51b1f7cabc
unknown <kuzma.boh@gmail.com>
2020-07-20 20:23:04 +0200

    Minor redux improvements, figma design coded for description component

--- src/pages/coffe/coffe.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Coffe = () => (
-
-    <h1>Kawa prażona raz</h1>
-)
-
-export default Coffe
--- src/pages/description/description.jsx
+++ src/pages/description/description.jsx
@@ -3,23 +3,48 @@ import './description.style.scss';
 import { connect } from 'react-redux';
 import { selectItemToDisplay } from '../../redux/description/description-selectors';
 import { ToggleDescriptionHidden } from '../../redux/description/description.actions';
+import { addItemToCartArray } from '../../redux/cart/cart.actions';
+import UniButton from '../../components/uniButton/uniButton';
+import { ReactComponent as Close } from '../../assets/remove.svg';
+import { motion } from 'framer-motion';
+import { containerVariants } from '../../animations/page-animations';
 
-const Description = ({ itemToPresent, ToggleDescriptionHidden}) => {
-    const { name, price, imageUrl, description } = itemToPresent
+const Description = ({ ToggleDescriptionHidden, addItemToCartArray, item}) => {
+    const { name, price, imageUrl, description } = item
     return (
     <section className="description">
-        <h2 className="description__title">{name}</h2>
-        <p className="description__text">{description}</p>
-        <button onClick={ToggleDescriptionHidden}>Zamknij</button>
+        <motion.div className="description__container" variants={containerVariants} initial="hidden" animate="visible" exit="exit">
+            <div className="description__container__image-box"
+                style={{
+                    backgroundImage: `url(${imageUrl})`,
+                }}
+            />
+            <div className="description__container__content">
+                <h2 className="description__container__content__title">{name}</h2>
+                <p className="description__container__content__text">{description}</p>
+                <button className="description__container__content__close"
+                    onClick={ToggleDescriptionHidden}>
+                    <Close className="description__container__content__close__icon"/>
+                </button>
+                <div className="description__container__content__footer">
+                    <p className="description__container__content__footer__price">{price} zł</p>
+                    <UniButton
+                        onClick={() => addItemToCartArray(item)}
+                    >Dodaj do koszyka
+                    </UniButton>
+                </div>
+            </div>
+        </motion.div>
     </section>
 )}
 
 const mapDispatchToProps = dispatch => ({
     ToggleDescriptionHidden: () => dispatch(ToggleDescriptionHidden()),
+    addItemToCartArray: item => dispatch(addItemToCartArray(item)),
 })
 
 const mapStateToProps = (state) => ({
-    itemToPresent: selectItemToDisplay(state),
+    item: selectItemToDisplay(state),
 })
 
 
